c9871f88e18e2088066825c208a1ad9726c90dac
baadillahnabil <nabil.baadillah@tanihub.com>
2021-12-11 21:29:05 +0700

    [ADD] implement findAll and findOne

--- src/donations/donations.service.ts
+++ src/donations/donations.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { CreateDonationInput } from './dto/create-donation.input';
+import { PrismaService } from '../../prisma/prisma.service';
+import { Prisma } from '@prisma/client';
 
 @Injectable()
 export class DonationsService {
+  constructor(private prisma: PrismaService) {}
+
   create(createDonationInput: CreateDonationInput) {
     return 'This action adds a new donation';
   }
 
   findAll() {
-    return [
-      {
-        exampleField: 1,
-      },
-    ];
+    return this.prisma.donation.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} donation`;
+  findOne(whereUniqueInput: Prisma.DonationWhereUniqueInput) {
+    return this.prisma.donation.findUnique({ where: whereUniqueInput });
   }
 }
